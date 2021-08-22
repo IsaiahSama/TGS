db887b28ce7f49ba7b0ec6545ebe2c505e9eb81f
Isaiah-Sama <zeldaplayergl@gmail.com>
2021-08-22 00:50:59 -0400

    Initial Commit

---
+++ getdata.py
@@ -0,0 +1,26 @@
+from requests import get
+from bs4 import BeautifulSoup
+from pprint import pprint, pformat
+URL = "https://smite.fandom.com/wiki/VGS_Cheat_Sheet"
+page = get(URL)
+soup = BeautifulSoup(page.text, "html.parser")
+
+tables = soup.select(".wikitable")
+
+command_values = {}
+for table in tables:
+    split_text = table.text.split("\n")
+    good_text = [word for word in split_text if word]
+
+    values = good_text[5:]
+    command_names = [value for value in values if "-" not in value and not value.lower().startswith("v") and len(value) > 2 and "nintendo" not in value.lower()]
+    command_keys = [value for value in values if value.lower().startswith("v") and "-" not in value]
+    if not command_names: continue
+    section_dict = dict(zip(command_keys, command_names))
+    command_values.update(section_dict)
+
+with open("all_commands.py", "w") as f:
+    f.write("# File containing all commands.\n")
+    f.write(f"commands = {pformat(command_values)}")
+
+print("Done")
---
+++ main.py
@@ -0,0 +1,56 @@
+# File to handle EVERYTHING
+import keyboard
+from all_commands import commands
+from os import system
+from pynput.keyboard import Listener
+from pprint import pprint
+from time import sleep
+
+class Main:
+    def __init__(self, commands:dict) -> None:
+        self.com = commands
+        self.pressed = ""
+
+    def run(self):
+        while True:
+            system("CLS")
+            print("Waiting for you to press V:")
+            self.listen_for_prompt()
+            valid = self.track_input()
+            if valid:
+                self.send_message()
+            self.pressed = ""
+
+    def listen_for_prompt(self):
+        keyboard.wait("v")
+
+    def track_input(self):
+        self.pressed += "V"
+        while True:
+            with Listener(on_press=self.on_press) as listener:
+                listener.join()
+            
+            print(self.pressed)
+            valid = [{command, text} for command, text in self.com.items() if self.pressed.upper() in command]
+
+            if not valid: return False
+            if len(valid) == 1: return True
+
+            pprint(valid)
+
+    def send_message(self):
+        message = self.com[self.pressed.upper()]
+        keyboard.press_and_release("ctrl+a, backspace")
+        sleep(0.3)
+        keyboard.write(message.strip("\""))
+        sleep(0.5)
+        keyboard.press_and_release("enter")
+
+    def on_press(self, key):
+        self.pressed += key.__str__().upper().strip("'")
+        return False
+
+
+main = Main(commands)
+if __name__ == "__main__":
+    main.run()
